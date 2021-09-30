8a0d3725df41f7f49c6df1486778e6a47ef7e4fc
erjagadish <91522594+erjagadish@users.noreply.github.com>
2021-09-30 10:31:51 +0530

    Add files via upload

---
+++ Day1_Day2 Assignment.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 2
+y = 5
+xy = 'Hey'
+print (x+y, xy)
+
+
+# In[2]:
+
+
+print("""My name is Rajath Kumar M.P.
+
+I love Python.""")
+
+
+# In[3]:
+
+
+string1 = 'World'
+print('Hello', string1)
+
+string2 = '!'
+print('Hello', string1, string2)
+
+
+# In[4]:
+
+
+print('Hello' + string1 + string2)
+
+
+# In[5]:
+
+
+print("Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug")
+
+
+# In[6]:
+
+
+print("I want \\n to be printed.")
+
+
+# In[7]:
+
+
+x = 10
+print(type(x))
+ 
+s = 'abc'
+print(type(s))
+ 
+value= True
+print(type(value))
+
+percent=90.00
+type(percent)
+
+
+# In[ ]:
+
+
+
+
